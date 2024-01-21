c0ecc057422827514f8ec886ca42a2b22a0855e7
Max van de Wiel <max@maxvandewiel.nl>
2024-01-21 17:53:13 +0100

    fix unit tests

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -12,15 +12,15 @@ export type ButtonProps = {
 const StyledButton = styled.button<ButtonProps>`
   border: 0;
   line-height: 1;
-  font-family: ${(props) => (props.theme.fonts[0] ? props.theme.fonts[0] : "Roboto")};
-  font-size: ${(props) => (props.theme.fontSizes.medium ? props.theme.fontSizes.medium : "2em")};
+  font-family: ${(props) => (props?.theme?.fonts ? props.theme.fonts[0] : "Roboto")};
+  font-size: ${(props) => (props?.theme?.fontSizes?.medium ? props.theme.fontSizes.medium : "2em")};
   cursor: pointer;
   font-weight: 700;
   font-weight: bold;
   border-radius: 10px;
   display: inline-block;
-  color: ${(props) => (props.primary ? (props.theme.colors.lightBlue ? props.theme.colors.lightBlue : "#FF5655") : (props.theme.colors.onyx ? props.theme.colors.onyx : "#f4c4c4"))};
-  background-color: ${(props) => (props.primary ? (props.theme.colors.onyx ? props.theme.colors.onyx : "#f4c4c4") : (props.theme.colors.lightBlue ? props.theme.colors.lightBlue : "#FF5655"))};
+  color: ${(props) => (props.primary ? (props?.theme?.colors?.lightBlue ? props.theme.colors.lightBlue : "#FF5655") : (props?.theme?.colors?.onyx ? props.theme.colors.onyx : "#f4c4c4"))};
+  background-color: ${(props) => (props.primary ? (props?.theme?.colors?.onyx ? props.theme.colors.onyx : "#f4c4c4") : (props?.theme?.colors?.lightBlue ? props.theme.colors.lightBlue : "#FF5655"))};
   padding: ${(props) =>
     props.size === "small"
       ? "7px 25px 8px"
--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -5,9 +5,9 @@ export type ContainerProps = {};
 const StyledContainer = styled.div`
     display: "flex";
     width: 100%;
-    border: ${(props) => `1px solid ${(props.theme.colors.onyx ? props.theme.colors.onyx : "#36313D")}`};
-    background-color: ${(props) => (props.theme.colors.lightBlue ? props.theme.colors.lightBlue : "#AFDBD2")};
-    font-family: ${(props) => (props.theme.fonts[0] ? props.theme.fonts[0] : "Roboto")};
+    border: ${(props) => `1px solid ${(props?.theme?.colors?.onyx ? props.theme.colors.onyx : "#36313D")}`};
+    background-color: ${(props) => (props?.theme?.colors?.lightBlue ? props.theme.colors.lightBlue : "#AFDBD2")};
+    font-family: ${(props) => (props?.theme?.fonts ? props.theme.fonts[0] : "Roboto")};
 `;
 
 const Container: React.FC<ContainerProps> = ({
